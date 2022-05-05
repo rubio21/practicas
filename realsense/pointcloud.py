55fb76fd1c901b8f049aaee7482a0691a54ec191
Alberto labo <albertorp2000@hotmail.com>
2022-05-05 17:41:46 +0200

    .

--- realsense/pointcloud.py
+++ realsense/pointcloud.py
@@ -20,43 +20,77 @@ def write_ply(fn, verts, colors):
     with open(fn, 'wb') as f:
         f.write((ply_header % dict(vert_num=len(verts))).encode('utf-8'))
         np.savetxt(f, verts, fmt='%f %f %f %d %d %d ')
-        
-imgL = cv.pyrDown(cv.imread(cv.samples.findFile('stereo/stereoLeft/imageL0.jpg')))  # downscale images for faster processing
-imgR = cv.pyrDown(cv.imread(cv.samples.findFile('stereo/stereoRight/imageR0.jpg')))
 
-# disparity range is tuned for 'aloe' image pair
-window_size = 3
-min_disp = 16
-num_disp = 112-min_disp
-stereo = cv.StereoSGBM_create(minDisparity = min_disp,
-    numDisparities = num_disp,
-    blockSize = 16,
-    P1 = 8*3*window_size**2,
-    P2 = 32*3*window_size**2,
-    disp12MaxDiff = 1,
-    uniquenessRatio = 10,
-    speckleWindowSize = 100,
-    speckleRange = 32
-)
-disp = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
-h, w = imgL.shape[:2]
-f = 0.8 * w  # guess for focal length
-Q = np.float32([[1, 0, 0, -0.5 * w],
-                [0, -1, 0, 0.5 * h],  # turn points 180 deg around x-axis,
-                [0, 0, 0, -f],  # so that y-axis looks up
-                [0, 0, 1, 0]])
-points = cv.reprojectImageTo3D(disp, Q)
-colors = cv.cvtColor(imgL, cv.COLOR_BGR2RGB)
-mask = disp > disp.min()
-out_points = points[mask]
-out_colors = colors[mask]
-out_fn = 'out.ply'
-write_ply(out_fn, out_points, out_colors)
-print('%s saved' % out_fn)
+# Matched block size. It must be an odd number >=1 . Normally, it should be somewhere in the 3..11 range.
+block_size = 11
+min_disp = -128
+max_disp = 128
+# Maximum disparity minus minimum disparity. The value is always greater than zero.
+# In the current implementation, this parameter must be divisible by 16.
+num_disp = max_disp - min_disp
+# Margin in percentage by which the best (minimum) computed cost function value should "win" the second best value to consider the found match correct.
+# Normally, a value within the 5-15 range is good enough
+uniquenessRatio = 5
+# Maximum size of smooth disparity regions to consider their noise speckles and invalidate.
+# Set it to 0 to disable speckle filtering. Otherwise, set it somewhere in the 50-200 range.
+speckleWindowSize = 200
+# Maximum disparity variation within each connected component.
+# If you do speckle filtering, set the parameter to a positive value, it will be implicitly multiplied by 16.
+# Normally, 1 or 2 is good enough.
+speckleRange = 2
+disp12MaxDiff = 0
 
-cv.imshow('left', imgL)
-cv.imshow('disparity', (disp - min_disp) / num_disp)
-cv.waitKey()
+for ii in range(9):
+    path_imgL = 'rectified/' + str(ii) + '.png'
+    path_imgR = 'rectified/' + str(ii + 1) + '.png'
+    imgL = cv.imread(cv.samples.findFile(path_imgL)) # downscale images for faster processing
+    imgR = cv.imread(cv.samples.findFile(path_imgR))
+
+    # Draw the rectified images
+    fig, axes = plt.subplots(1, 2, figsize=(15, 10))
+    axes[0].imshow(imgL, cmap="gray")
+    axes[1].imshow(imgR, cmap="gray")
+    axes[0].axhline(250)
+    axes[1].axhline(250)
+    axes[0].axhline(450)
+    axes[1].axhline(450)
+    plt.suptitle("Rectified images")
+    plt.show()
+
+    stereo = cv.StereoSGBM_create(
+        minDisparity=min_disp,
+        numDisparities=num_disp,
+        blockSize=block_size,
+        uniquenessRatio=uniquenessRatio,
+        speckleWindowSize=speckleWindowSize,
+        speckleRange=speckleRange,
+        disp12MaxDiff=disp12MaxDiff,
+        P1=8 * 1 * block_size * block_size,
+        P2=32 * 1 * block_size * block_size,
+    )
+    disp = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
+
+    print('generating 3d point cloud...', )
+    h, w = imgL.shape[:2]
+    f = 0.8 * w  # guess for focal length
+    Q = np.float32([[1, 0, 0, -0.5 * w],
+                    [0, -1, 0, 0.5 * h],  # turn points 180 deg around x-axis,
+                    [0, 0, 0, -f],  # so that y-axis looks up
+                    [0, 0, 1, 0]])
+    points = cv.reprojectImageTo3D(disp, Q)
+    colors = cv.cvtColor(imgL, cv.COLOR_BGR2RGB)
+    mask = disp > disp.min()
+    out_points = points[mask]
+    out_colors = colors[mask]
+    out_fn = 'out.ply'
+    write_ply(out_fn, out_points, out_colors)
+    print('%s saved' % out_fn)
+
+    cv.imshow('left', imgL)
+    cv.imshow('disparity', (disp - min_disp) / num_disp)
+    cv.waitKey()
+
+    print('Done')
 
 print('Done')
 
